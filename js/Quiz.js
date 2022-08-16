3267a3bb29451f4cf1a38d3c09cd0729f0e0b373
Christopher Hullman <christopherhullman@gmail.com>
2022-08-16 13:10:49 +1000

    seperated out element creation code in quiz.js into functions

--- js/Quiz.js
+++ js/Quiz.js
@@ -2,31 +2,49 @@ class Quiz {
   constructor(pokemonData) {
     this._pokemonData = pokemonData;
     // Create root overlay element:
-    this._quizOverlayEl = document.createElement('div');
-    this._quizOverlayEl.classList.add('quiz-overlay');
-    document.body.appendChild(this._quizOverlayEl);
+    this._quizOverlayEl = this._generateRootOverlay();
     // Create quiz element:
+    this._quizEl = this._generateQuizElement();
+    // Render Pokemon image:
+    this._renderPokemonImage();
+    // Create abort button:
+    this._generateAbortButton();
+  }
+
+  start() {
+
+  }
+
+  _generateRootOverlay() {
+    const _quizOverlayEl = document.createElement('div');
+    _quizOverlayEl.classList.add('quiz-overlay');
+    document.body.appendChild(_quizOverlayEl);
+    return _quizOverlayEl;
+  }
+  
+  _generateQuizElement() {
     const quizEl = document.createElement('div');
     quizEl.classList.add('quiz');
     this._quizOverlayEl.appendChild(quizEl);
-    // Render Pokemon image:
+    return quizEl;
+  }
+
+  _renderPokemonImage() {
     const pokemonImg = document.createElement('img');
     pokemonImg.classList.add('sprite');
     pokemonImg.src = this._pokemonData.sprites.front_default;
     pokemonImg.alt = 'Pokemon Image';
-    quizEl.appendChild(pokemonImg);
-    // Create abort button:
+    this._quizEl.appendChild(pokemonImg);
+  }
+
+  _generateAbortButton() {
     const abortButton = document.createElement('button');
     abortButton.classList.add('abort');
     abortButton.textContent = 'Abort Quiz';
     abortButton.addEventListener('click', () => {
       this._quizOverlayEl.remove();
     });
-    quizEl.appendChild(abortButton);
-  }
-
-  start() {
-
+    this._quizEl.appendChild(abortButton);
   }
 }
 
