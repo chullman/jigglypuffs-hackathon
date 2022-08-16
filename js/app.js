05e43ae6978ad7e999487aef820b791e4b032981
Christopher Hullman <christopherhullman@gmail.com>
2022-08-16 15:29:13 +1000

    Added a bunch of styling to About page. in app.js, changed the DOM selector for the start game link to classes, instead of ids, as there are multiple start game buttons throughout the website

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import Map from './Map.js';
 import Game from './Game.js';
 
-const startGameLink = document.getElementById('start-game-link');
+const startGameLinks = document.getElementsByClassName('start-game-link');
 const aboutLink = document.getElementById('about-link');
 const introLink = document.getElementById('intro-link');
 
@@ -9,13 +9,15 @@ const intro = document.querySelector('.intro');
 const about = document.querySelector('.about');
 const game = document.querySelector('.game');
 
-startGameLink.addEventListener('click', () => {
-  intro.style.display = 'none';
-  about.style.display = 'none';
-  game.style.display = 'flex';
-
-  startGame();
-});
+for (let startGameLink of startGameLinks) {
+  startGameLink.addEventListener('click', () => {
+    intro.style.display = 'none';
+    about.style.display = 'none';
+    game.style.display = 'flex';
+  
+    startGame();
+  });
+}
 
 aboutLink.addEventListener('click', () => {
   intro.style.display = 'none';
